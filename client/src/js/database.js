76fb956a2a8076bad37f64cc4022990d534a1d2f
mdinkelbach <mj.dinkelbach@mgail.com>
2023-03-07 11:26:09 -0600

    sw

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,24 +12,21 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.error('putDb not implemented');
-  const IndexedDb = await openDB('index', 1);
-  const tx = IndexedDb.transaction('index', 'readwrite');
-  const store = tx.objectStore('index');
-  const request = store.put(content);
+  const IndexedDb = await openDB('jate', 1);
+  const tx = IndexedDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({content: content});
   const result = await request;
   console.log('result.value', result);
-  return result?.value;
 }
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.error('getDb not implemented');
-  const IndexedDb = await openDB('index', 1);
-  const tx = IndexedDb.transaction('index', 'readonly');
-  const store = tx.objectStore('index');
+  const IndexedDb = await openDB('jate', 1);
+  const tx = IndexedDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
